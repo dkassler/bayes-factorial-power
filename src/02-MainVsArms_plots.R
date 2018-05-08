4e9ee3f1a9d6b0f1e8fe526322316d37006b093c
DKassler <dkassler@mathematica-mpr.com>
2018-05-08 15:42:39 -0400

    different options for main vs arm plots

--- src/02-MainVsArms_plots.R
+++ src/02-MainVsArms_plots.R
@@ -1,7 +1,14 @@
+setwd(here::here())
+library(ProjectTemplate)
+load.project()
+
 sims <- readRDS(
   file.path("N:", "Project", "50381_PCI-W", "DC1", "Factorial Design paper", 
             "Data", "mde_full.Rds")
 )
+
+
+# Facetted Boxplot --------------------------------------------------------
 
 boxplot_data <- sims %>% 
   mutate(nArms = factor(nA * nB * nC * nD)) %>% 
@@ -10,7 +17,7 @@ boxplot_data <- sims %>%
   ungroup() %>% 
   replace_na(list(MDE = 1))
 
-ggplot(data = boxplot_data) +
+boxplot_plot <- ggplot(data = boxplot_data) +
   facet_grid(N ~ nArms) +
   geom_boxplot(aes(x = sigfun, y = MDE, color = pct_na > 1/3)) +
   scale_x_discrete(limits = c('bayes_arms', 'freq_arms', 
@@ -23,6 +30,44 @@ ggplot(data = boxplot_data) +
                      values = c('black', 'brown')) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
   labs(x = "Significance Method")
+boxplot_plot
 
-ggsave('graphs/boxplot_arms_main.png')
+ggsave('graphs/boxplot_arms_main.png', width = 6, height = 6)
+  
+
+# Facetted bar plot -------------------------------------------------------
+
+pointplot_data <- sims %>% 
+  mutate(nArms = factor(nA * nB * nC * nD)) %>% 
+  group_by(N, nArms, bayes, efftype, sigfun) %>% 
+  summarize(pct_na = mean(is.na(MDE)),
+            mean_MDE = mean(MDE, na.rm = TRUE),
+            median_MDE = median(replace_na(MDE, Inf))) %>% 
+  ungroup() %>% 
+  filter(pct_na < 1/3)
+
+pointplot_labeller <- labeller(bayes = c('TRUE' = 'Bayesian',
+                                         'FALSE' = 'Frequentist'))
+
+ggplot(data = pointplot_data,
+       aes(x = N, 
+           color = sigfun,
+           shape = efftype,
+           linetype = bayes,
+           y = median_MDE)) +
+  facet_grid(. ~ nArms, labeller = pointplot_labeller) +
+  geom_point() +
+  geom_line() +
+  scale_color_discrete(limits = c('bayes_arms', 'freq_arms', 
+                              'bayes_main', 'freq_main'),
+                   breaks = c('bayes_arms', 'bayes_main',
+                              'freq_arms', 'freq_main'),
+                   labels = c('Bayes Arms', 'Bayes Main Eff.',
+                              'Freq. Arms', 'Freq. Main Eff.'))
+
+
+# Combined boxplot --------------------------------------------------------
+
+boxplot_plot +
+  geom_step(aes(x = sigfun, y = MDE))
   
