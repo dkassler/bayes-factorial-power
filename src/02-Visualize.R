fb981e47d1b0441159860b00f1e4e0e52890ef8f
Daniel <dlotzk@gmail.com>
2018-05-16 18:43:01 -0400

    lineplot with proper labels for comparison bayes freq

--- src/02-Visualize.R
+++ src/02-Visualize.R
@@ -7,30 +7,50 @@ sims <- readRDS("../mde_full.Rds")
 # Boxplot -----------------------------------------------------------------
 
 boxplot_data <- sims %>% 
+  filter(sigfun == 'bayes_arms') %>%
   mutate(nArms = factor(nA * nB * nC * nD)) %>% 
   replace_na(list(MDE = 1))
 
 ggplot(data = boxplot_data) +
-  facet_wrap(~sigfun) + 
-  geom_boxplot(aes(x = factor(N), fill = nArms, y = MDE))
-
-ggsave('graphs/boxplot.png')
+  # facet_wrap(~sigfun) + 
+  geom_boxplot(aes(x = factor(N), fill = nArms, y = MDE)) +
+  theme_bw() +
+  ggsave('graphs/boxplot.png')
 
 # Line plot median MDE ----------------------------------------------------
 
 lineplot_data <- sims %>% 
+  mutate(bayes = factor(bayes, levels = c(TRUE, FALSE),
+                        labels = c('Bayesian', 'Classical'))) %>% 
   mutate(nArms = factor(nA * nB * nC * nD)) %>%
   group_by(N, nArms, sigfun, bayes, efftype) %>% 
   summarise(MDE = if (mean(is.na(MDE)) > 1/3) NA else 
     median(replace_na(MDE, Inf)))
 
 lineplot <- function(.x) ggplot(data = lineplot_data %>% filter(efftype == .x), 
-       aes(x = N, y = MDE, color = nArms, shape = bayes, linetype = bayes)) +
-  geom_point() + 
-  geom_line()
+                                aes(x = N, y = MDE)) +  
+    geom_line(aes(col=factor(nArms, levels=c(16,72,180,250), 
+                             labels=c('16 arms',
+                                      '72 arms',
+                                      '180 arms',
+                                      '250 arms')),
+                  linetype = bayes)) +
+    geom_point(aes(col=factor(nArms, levels=c(16,72,180,250), 
+                              labels=c('16 arms',
+                                       '72 arms',
+                                       '180 arms',
+                                       '250 arms')),
+                   shape = bayes))+
+    guides(col=guide_legend(title=''), linetype = guide_legend(title = ''),
+           shape = guide_legend(title = '')) +
+    labs(x='Sample size') +
+    theme_bw() + 
+    scale_color_discrete(breaks = c('16 arms','72 arms','180 arms','250 arms'))+
+    scale_y_continuous(limits = c(0, .7)) + 
+    scale_x_continuous(limits = c(0, 10000)) +
+  ggsave(sprintf('graphs/lineplot_%s.png', .x), width = 6.5, height = 5)
+
+lineplot1 <- function(.x) lineplot()
 
 lineplot('arms')
-ggsave('graphs/lineplot_arms.png')
-
 lineplot('main')
-ggsave('graphs/lineplot_main.png')
